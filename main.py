ed07cab8f13dfc52e208fd8a67f5a8ae767a492a
Mohd Irfan <mohdirfan70097@gmail.com>
2025-05-20 16:33:13 +0530

    Updating

--- main.py
+++ main.py
@@ -4,7 +4,25 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    return render_template("home.html")
+
+
+@app.route("/service")
+def service():
+    return render_template("services.html")
+
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
 
 
 app.run(debug=True)
